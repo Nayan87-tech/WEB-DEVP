fa1f1d6f9f2a64db7b4b4bc7f5458771641e8bd7
Nayan87-tech <nayangairola1@gmail.com>
2026-04-15 23:03:48 +0530

    practice

---
+++ javascript/questions.js
@@ -0,0 +1,54 @@
+// Object:-
+// create obj called person...properties name,age,city!
+
+// let person={
+//     name:"nayan",
+//     age:18,
+//     city:"Delhi",
+// }
+// console.log(person);
+
+// ---------------------------------
+
+// Array:-
+// create array of 5 number
+// print first and last element
+// find length of an array
+
+// let a = [1,2,3,4,5];
+// console.log(a[0],a[4]);
+// console.log(a.length);
+
+// ----------------------------------
+
+// Functios:-
+// using function print "hello world"
+// using function take two values and print their +,-,*,/ ...
+
+
+function hello(){
+    console.log("Hello World");
+}
+hello()
+
+function maths(a,b){
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+}
+maths(10,2);
+
+function square(a){
+    return `square of a number is :  ${a*a}`
+}
+console.log(square(10));
+
+
+
+
+
+
+
+
+
--- javascript/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-{
-//     return`multiplication of a and b is: ${a*b}`
-// }
-// console.log(multiply(2,10));
